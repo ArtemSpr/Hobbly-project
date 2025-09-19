873ecde3eb9a96fc557857f2f9a54d3c4fe4f584
WellNoteOne <165422516+WellNoteOne@users.noreply.github.com>
2025-09-19 20:38:36 +0300

    small update of map

--- frontend/frontend-app/src/pages/map/map.jsx
+++ frontend/frontend-app/src/pages/map/map.jsx
@@ -22,48 +22,52 @@ const Map = () => {
   console.log("Got locations:", locations);
   return (
     <div className="map">
-      <MapContainer
-        center={[60.17, 24.93]} // Helsinki
-        zoom={12} // default zoom level
-        minZoom={11} // minimum zoom level
-        maxZoom={17} // maximum zoom level
-        maxBounds={[
-          [59.8, 24.4],
-          [60.5, 25.5],
-        ]} // bounding box for the map
-        maxBoundsViscosity={1.0} // "stickiness" to bounds
-        scrollWheelZoom={true} // zoom with mouse wheel
-        doubleClickZoom={true} // zoom with double-click
-        dragging={true} // drag the map
-        zoomControl={true} // show zoom control buttons
-        touchZoom={true} // zoom on touch devices
-        preferCanvas={true} // performance optimization for many markers
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2VsbG5vdGUiLCJhIjoiY205ODZuODJqMGV4eDJsc2VwZ3hraHAzaSJ9.Ei8toDcIcpyPbVlu6BLkvw"
-          attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a>'
-          id="mapbox/streets-v11"
-          tileSize={512}
-          zoomOffset={-1}
-        />
-        <MarkerClusterGroup spiderfyOnClick showCoverageOnHover>
-          {locations
-            .filter((loc) => loc.latitude && loc.longitude)
-            .map((loc) => (
-              <Marker
-                key={loc.id}
-                position={[loc.latitude, loc.longitude]}
-                icon={iconMark}
-              >
-                <Popup>{loc.name}</Popup>
-              </Marker>
-            ))}
-        </MarkerClusterGroup>
-      </MapContainer>
-      <Link to="/navigation" className="back-button">
-        &lt;
-      </Link>
+      <div className="map-cont">
+        <MapContainer
+          center={[60.17, 24.93]} // Helsinki
+          zoom={12} // default zoom level
+          minZoom={11} // minimum zoom level
+          maxZoom={17} // maximum zoom level
+          maxBounds={[
+            [59.8, 24.4],
+            [60.5, 25.5],
+          ]} // bounding box for the map
+          maxBoundsViscosity={1.0} // "stickiness" to bounds
+          scrollWheelZoom={true} // zoom with mouse wheel
+          doubleClickZoom={true} // zoom with double-click
+          dragging={true} // drag the map
+          zoomControl={true} // show zoom control buttons
+          touchZoom={true} // zoom on touch devices
+          preferCanvas={true} // performance optimization for many markers
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2VsbG5vdGUiLCJhIjoiY205ODZuODJqMGV4eDJsc2VwZ3hraHAzaSJ9.Ei8toDcIcpyPbVlu6BLkvw"
+            attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+            id="mapbox/streets-v11"
+            tileSize={512}
+            zoomOffset={-1}
+          />
+          <MarkerClusterGroup spiderfyOnClick showCoverageOnHover>
+            {locations
+              .filter((loc) => loc.latitude && loc.longitude)
+              .map((loc) => (
+                <Marker
+                  key={loc.id}
+                  position={[loc.latitude, loc.longitude]}
+                  icon={iconMark}
+                >
+                  <Popup>{loc.name}</Popup>
+                </Marker>
+              ))}
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
+      <div className="but">
+        <Link to="/navigation" className="back-button">
+          &lt;
+        </Link>
+      </div>
     </div>
   );
 };
